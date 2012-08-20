459bec00d75f7a908b5f7f44b5f1a81fa8b8eea0
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2012-08-20 13:09:41 +0200

    OSPD-186, OSPD-187: Adding GeneratedTitle and GeneratedDescription properties to SeoPart

--- Handlers/SeoPartHandler.cs
+++ Handlers/SeoPartHandler.cs
@@ -6,12 +6,24 @@ using JetBrains.Annotations;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Data;
 using Onestop.Seo.Models;
+using Orchard.Environment;
+using Onestop.Seo.Services;
 
 namespace Onestop.Seo.Handlers {
     [UsedImplicitly]
     public class SeoPartHandler : ContentHandler {
-        public SeoPartHandler(IRepository<SeoPartRecord> repository) {
+        public SeoPartHandler(
+            IRepository<SeoPartRecord> repository,
+            Work<ISeoService> seoServiceWork) {
             Filters.Add(StorageFilter.For(repository));
+
+            OnActivated<SeoPart>((context, part) => {
+                part.GeneratedTitleField.Loader(() => seoServiceWork.Value.GenerateTitle(part.ContentItem));
+            });
+
+            OnActivated<SeoPart>((context, part) => {
+                part.GeneratedDescriptionField.Loader(() => seoServiceWork.Value.GenerateDescription(part.ContentItem));
+            });
         }
     }
 }
--- Models/SeoPart.cs
+++ Models/SeoPart.cs
@@ -6,9 +6,22 @@ using Orchard.ContentManagement.Records;
 using Orchard.ContentManagement;
 using System.ComponentModel.DataAnnotations;
 using Orchard.Data.Conventions;
+using Orchard.Core.Common.Utilities;
 
 namespace Onestop.Seo.Models {
     public class SeoPart : ContentPart<SeoPartRecord> {
+        private readonly LazyField<string> _generatedTitle = new LazyField<string>();
+        public LazyField<string> GeneratedTitleField { get { return _generatedTitle; } }
+        public string GeneratedTitle {
+            get { return _generatedTitle.Value; }
+        }
+
+        private readonly LazyField<string> _generatedDescription = new LazyField<string>();
+        public LazyField<string> GeneratedDescriptionField { get { return _generatedDescription; } }
+        public string GeneratedDescription {
+            get { return _generatedDescription.Value; }
+        }
+
         public string TitleOverride {
             get { return Record.TitleOverride; }
             set { Record.TitleOverride = value; }
