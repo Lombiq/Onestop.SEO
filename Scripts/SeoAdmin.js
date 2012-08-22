b4d23d0c88b30191658a3b14a108d056d015b506
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2012-08-22 20:13:41 +0200

    OSPD-186, OSPD-187: character counter and warning for description and title rewriter

--- Scripts/SeoAdmin.js
+++ Scripts/SeoAdmin.js
@@ -6,21 +6,34 @@
                     $(".onestop-seo-rewrite-value").each(function (index) {
                         var textBox = $(this);
                         var defaultValue = textBox.attr("data-generated-default");
+                        var characterCounter = textBox.next(".onestop-seo-character-counter");
+
+                        characterCounter.val(textBox.val().length);
 
                         if (textBox.val() == defaultValue) {
                             textBox.addClass("onestop-seo-generated-default");
                         }
 
-                        $(this).focus(function () {
+                        textBox.focus(function (e) {
                             if (textBox.val() != defaultValue) return;
                             textBox.removeClass("onestop-seo-generated-default");
                             textBox.val("");
                         });
 
-                        $(this).blur(function () {
+                        textBox.blur(function (e) {
                             if (textBox.val() != "" && textBox.val() != defaultValue) return;
                             textBox.addClass("onestop-seo-generated-default");
                             textBox.val(defaultValue);
+                        });
+
+                        textBox.keyup(function (e) {
+                            var length = textBox.val().length;
+                            var maxLength = textBox.attr("data-max-length");
+
+                            if (length > maxLength) textBox.addClass("onestop-seo-too-long");
+                            else textBox.removeClass("onestop-seo-too-long");
+
+                            characterCounter.val(length);
                         });
                     });
                 }
