11c0fa2badf544930ff00804e6a3bc18c37b554e
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2012-10-04 17:21:02 +0200

    OSPD-186: Starting with tabs

---
+++ Routes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace Onestop.Seo {
+    public class Routes : IRouteProvider {
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes()) {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[]
+                {
+                    new RouteDescriptor
+                        {
+                            Priority = 100, 
+                            Route =
+                                new Route(
+                                "Onestop.Seo/Rewriter/{rewriterType}", 
+                                new RouteValueDictionary
+                                    {
+                                        { "area", "Onestop.Seo" }, 
+                                        { "controller", "Admin" }, 
+                                        { "action", "Rewriter" }
+                                    }, 
+                                new RouteValueDictionary(), 
+                                new RouteValueDictionary { { "area", "Onestop.Seo" } }, 
+                                new MvcRouteHandler())
+                        }
+                };
+        }
+    }
+}
