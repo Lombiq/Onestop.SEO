702e3400fe6968940443c9264184bd1219a24020
Jorge Agraz <jorge.agraz@onestop.com>
2015-07-30 16:58:08 -0700

    COM-156: update the cache logic to remove stupidity

--- Services/SeoSettingsManager.cs
+++ Services/SeoSettingsManager.cs
@@ -18,18 +18,17 @@ namespace Onestop.Seo.Services {
 
 
         public ISeoGlobalSettings GetGlobalSettings() {
-            var id = _cacheService.Get(CacheKey, () => {
-                var settings = _contentManager.Query(VersionOptions.Latest, "SeoSettings").Join<SeoGlobalSettingsPartRecord>().Slice(0, 1).FirstOrDefault();
-                
-                if (settings == null) {
-                    settings = _contentManager.New("SeoSettings");
-                    _contentManager.Create(settings);
-                }
-                
-                return settings.Id;
-            });
 
-            return _contentManager.Get(id, VersionOptions.Latest, new QueryHints().ExpandRecords<SeoGlobalSettingsPartRecord>()).As<SeoGlobalSettingsPart>();
+            var settings = _contentManager.Query(VersionOptions.Latest, "SeoSettings").Join<SeoGlobalSettingsPartRecord>().Slice(0, 1).FirstOrDefault();
+
+            if (settings == null)
+            {
+                settings = _contentManager.New("SeoSettings");
+                _contentManager.Create(settings);
+
+            }
+
+            return _contentManager.Get(settings.Id, VersionOptions.Latest, new QueryHints().ExpandRecords<SeoGlobalSettingsPartRecord>()).As<SeoGlobalSettingsPart>();
         }
     }
 }
