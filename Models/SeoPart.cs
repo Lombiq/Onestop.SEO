e5a111350d953f3d667cd7024c2b331c8094c5bb
jorge.agraz@onestop.com <jorge.agraz@onestop.com>
2015-11-05 22:19:37 -0800

    EXM-343: -wire up migrations and menu stuff for canonicals and HTML cards

--- Models/SeoPart.cs
+++ Models/SeoPart.cs
@@ -5,47 +5,67 @@ using Orchard.Core.Common.Utilities;
 using Orchard.Data.Conventions;
 
 namespace Onestop.Seo.Models {
-    public class SeoPart : ContentPart<SeoPartRecord> {
+    public class SeoPart : ContentPart<SeoPartRecord>
+    {
         private readonly LazyField<ISeoGlobalSettings> _globalSettings = new LazyField<ISeoGlobalSettings>();
         public LazyField<ISeoGlobalSettings> GlobalSettingsField { get { return _globalSettings; } }
-        public ISeoGlobalSettings GlobalSettings {
+        public ISeoGlobalSettings GlobalSettings
+        {
             get { return _globalSettings.Value; }
         }
 
         private readonly LazyField<string> _generatedTitle = new LazyField<string>();
         public LazyField<string> GeneratedTitleField { get { return _generatedTitle; } }
-        public string GeneratedTitle {
+        public string GeneratedTitle
+        {
             get { return _generatedTitle.Value; }
         }
 
         private readonly LazyField<string> _generatedDescription = new LazyField<string>();
         public LazyField<string> GeneratedDescriptionField { get { return _generatedDescription; } }
-        public string GeneratedDescription {
+        public string GeneratedDescription
+        {
             get { return _generatedDescription.Value; }
         }
 
         private readonly LazyField<string> _generatedKeywords = new LazyField<string>();
         public LazyField<string> GeneratedKeywordsField { get { return _generatedKeywords; } }
-        public string GeneratedKeywords {
+        public string GeneratedKeywords
+        {
             get { return _generatedKeywords.Value; }
         }
 
-        public string TitleOverride {
+        public string TitleOverride
+        {
             get { return Record.TitleOverride; }
             set { Record.TitleOverride = value; }
         }
 
-        public string DescriptionOverride {
+        public string DescriptionOverride
+        {
             get { return Record.DescriptionOverride; }
             set { Record.DescriptionOverride = value; }
         }
 
-        public string KeywordsOverride {
+        public string KeywordsOverride
+        {
             get { return Record.KeywordsOverride; }
             set { Record.KeywordsOverride = value; }
         }
-    }
 
+        public string CanonicalUrlOverride
+        {
+            get { return Record.CanonicalUrlOverride; }
+            set { Record.CanonicalUrlOverride = value; }
+        }
+
+        public string HTMLCardOverride
+        {
+            get { return Record.HTMLCardOverride; }
+            set { Record.HTMLCardOverride = value; }
+
+        }
+    }
     public class SeoPartRecord : ContentPartVersionRecord {
         [StringLength(1024)]
         public virtual string TitleOverride { get; set; }
@@ -55,5 +75,11 @@ namespace Onestop.Seo.Models {
 
         [StringLengthMax]
         public virtual string KeywordsOverride { get; set; }
+
+        [StringLength(1024)]
+        public virtual string CanonicalUrlOverride { get; set; }
+        [StringLength(2048)]
+        public virtual string HTMLCardOverride { get; set; }
+
     }
 }
