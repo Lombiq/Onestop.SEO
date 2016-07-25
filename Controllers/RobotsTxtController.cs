c345165445c95c5d97202e679e6e2b8b53088fce
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-07-26 00:19:03 +0200

    Simplifying RobotsTxtController
    --HG--
    branch : issue/LMBQ-43

--- Controllers/RobotsTxtController.cs
+++ Controllers/RobotsTxtController.cs
@@ -7,33 +7,20 @@ using System.Web.Mvc;
 namespace Onestop.Seo.Controllers {
     public class RobotsTxtController : Controller {
         private readonly ISeoSettingsManager _seoSettingsManager;
-        private readonly Work<ICurrentContentService> _currentContentServiceWork;
-        private readonly Work<ITokenizer> _tokenizerWork;
+        private readonly ITokenizer _tokenizer;
 
         public RobotsTxtController(
             ISeoSettingsManager seoSettingsManager,
-            Work<ICurrentContentService> currentContentServiceWork,
-            Work<ITokenizer> tokenizerWork) {
+            ITokenizer tokenizer) {
             _seoSettingsManager = seoSettingsManager;
-            _currentContentServiceWork = currentContentServiceWork;
-            _tokenizerWork = tokenizerWork;
+            _tokenizer = tokenizer;
         }
 
         public string Index() {
-            var globalSettingsPart = _seoSettingsManager.GetGlobalSettings();
-
-            return Tokenize(globalSettingsPart.RobotsTxtText);
-        }
-
-        private string Tokenize(string pattern) {
-            Dictionary<string, object> replaceData = null;
-            var currentContent = _currentContentServiceWork.Value.GetContentForRequest();
-            if (currentContent != null) replaceData = new Dictionary<string, object> { { "Content", currentContent } };
-
-            return _tokenizerWork.Value.Replace(
-                            pattern,
-                            replaceData,
-                            new ReplaceOptions { Encoding = ReplaceOptions.NoEncode });
+            return _tokenizer.Replace(
+                _seoSettingsManager.GetGlobalSettings().RobotsTxtText,
+                new Dictionary<string, object>(),
+                new ReplaceOptions { Encoding = ReplaceOptions.NoEncode });
         }
     }
 }
