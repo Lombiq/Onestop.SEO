9f8a9c25339e70d72b7986df22f87b48ec04a05f
Bálint Réthy <balint.rethy@lombiq.com>
2016-07-25 15:11:09 +0200

    Adding text tokenizer
    --HG--
    branch : issue/LMBQ-43

--- Controllers/RobotsTxtController.cs
+++ Controllers/RobotsTxtController.cs
@@ -1,20 +1,39 @@
 ﻿using Onestop.Seo.Services;
+using Orchard.Environment;
+using Orchard.Tokens;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace Onestop.Seo.Controllers {
     public class RobotsTxtController : Controller {
         private readonly ISeoSettingsManager _seoSettingsManager;
+        private readonly Work<ICurrentContentService> _currentContentServiceWork;
+        private readonly Work<ITokenizer> _tokenizerWork;
 
-
-        public RobotsTxtController(ISeoSettingsManager seoSettingsManager) {
+        public RobotsTxtController(
+            ISeoSettingsManager seoSettingsManager,
+            Work<ICurrentContentService> currentContentServiceWork,
+            Work<ITokenizer> tokenizerWork) {
             _seoSettingsManager = seoSettingsManager;
+            _currentContentServiceWork = currentContentServiceWork;
+            _tokenizerWork = tokenizerWork;
         }
-
 
         public string Index() {
             var globalSettingsPart = _seoSettingsManager.GetGlobalSettings();
 
-            return globalSettingsPart.RobotsTxtText;
+            return Tokenize(globalSettingsPart.RobotsTxtText);
+        }
+
+        private string Tokenize(string pattern) {
+            Dictionary<string, object> replaceData = null;
+            var currentContent = _currentContentServiceWork.Value.GetContentForRequest();
+            if (currentContent != null) replaceData = new Dictionary<string, object> { { "Content", currentContent } };
+
+            return _tokenizerWork.Value.Replace(
+                            pattern,
+                            replaceData,
+                            new ReplaceOptions { Encoding = ReplaceOptions.NoEncode });
         }
     }
 }
